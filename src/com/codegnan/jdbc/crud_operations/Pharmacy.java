cbd97281d7a9caf2a1438de82feb5935c32f667d
sathwikkaa <146510482+sathwikkaa@users.noreply.github.com>
2026-02-24 13:28:54 +0530

    Pharmacy.java

--- src/com/codegnan/jdbc/crud_operations/Pharmacy.java
+++ src/com/codegnan/jdbc/crud_operations/Pharmacy.java
@@ -2,25 +2,37 @@ package com.codegnan.jdbc.crud_operations;
 
 public class Pharmacy {
 
-    private int medicineId;
-    private String medicineName;
+    
+    public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	private String medicineName;
     private String category;
     private double price;
 
-    public Pharmacy(int medicineId, String medicineName, String category, double price) {
-        this.medicineId = medicineId;
+    public Pharmacy( String medicineName, String category, double price) {
+       
         this.medicineName = medicineName;
         this.category = category;
         this.price = price;
     }
 
-    public int getMedicineId() { return medicineId; }
-    public String getMedicineName() { return medicineName; }
-    public String getCategory() { return category; }
+    
+    public String getName() { return medicineName; }
+    public String getType() { return category; }
     public double getPrice() { return price; }
 
     @Override
     public String toString() {
-        return medicineId + " " + medicineName + " " + category + " " + price;
+        return  medicineName + " " + category + " " + price;
     }
 }
